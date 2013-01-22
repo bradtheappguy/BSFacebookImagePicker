9d49d864aeff3e943af6ea3c2961734cce9e572c
Brad Smith <bradsmithinc@gmail.com>
2013-01-22 13:57:28 -0800

    Initial Commit

---
+++ FBAlbumPicker/CXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CXAppDelegate.h
+//  FBAlbumPicker
+//
+//  Created by Brad Smith on 1/22/13.
+//  Copyright (c) 2013 Brad Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FBAlbumPicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FBAlbumPicker
+//
+//  Created by Brad Smith on 1/22/13.
+//  Copyright (c) 2013 Brad Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([CXAppDelegate class]));
+  }
+}
