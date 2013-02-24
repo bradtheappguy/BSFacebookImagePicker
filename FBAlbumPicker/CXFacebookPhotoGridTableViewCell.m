df8d92867b1b47b694453ed94542cc26b5d293b9
Brad Smith <bradsmithinc@gmail.com>
2013-02-23 20:32:25 -0800

    Set content mode properly on photo grid

--- FBAlbumPicker/CXFacebookPhotoGridTableViewCell.m
+++ FBAlbumPicker/CXFacebookPhotoGridTableViewCell.m
@@ -18,12 +18,15 @@
 #import "AFNetworking.h"
 #import "ImageCropViewController.h"
 
+static CGFloat kThumbnailLength = 75;
+static CGFloat kThumbnailMargin = 4;
+static NSUInteger kNumberOfImages = 4;
+
+
 @implementation CXFacebookPhotoGridTableViewCell
 @synthesize navigationController;
 @synthesize delegate;
 
-static CGFloat kThumbnailLength = 75;
-static CGFloat kThumbnailMargin = 4;
 
 - (id)initWithReuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -32,12 +35,15 @@ static CGFloat kThumbnailMargin = 4;
       self.selectionStyle = UITableViewCellSelectionStyleNone;
       
       _imageViews = [[NSMutableArray alloc] initWithCapacity:4];
-      for (int c = 0; c < 4;c++) {
+      for (int c = 0; c < kNumberOfImages ;c++) {
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake((kThumbnailMargin*(c+1)) + (kThumbnailLength*c), 2, kThumbnailLength, kThumbnailLength)];
+        imageView.clipsToBounds = YES;
+        imageView.contentMode = UIViewContentModeScaleAspectFill;
         imageView.backgroundColor = [UIColor lightGrayColor];
         [self.contentView addSubview:imageView];
         imageView.tag = c;
         UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(imageViewPressed:)];
+        tap.delegate = self;
         [imageView addGestureRecognizer:tap];
         imageView.userInteractionEnabled = YES;
         [_imageViews addObject:imageView];
@@ -55,21 +61,17 @@ static CGFloat kThumbnailMargin = 4;
 
 - (void)setImages:(NSArray *)images {
   _images = images;
-
-  if (images.count >= 1) [_imageViews[0] setImageWithURL:[NSURL URLWithString:images[0][@"picture"]]];
-  if (images.count >= 2) [_imageViews[1] setImageWithURL:[NSURL URLWithString:images[1][@"picture"]]];
-  if (images.count >= 3) [_imageViews[2] setImageWithURL:[NSURL URLWithString:images[2][@"picture"]]];
-  if (images.count >= 4) [_imageViews[3] setImageWithURL:[NSURL URLWithString:images[3][@"picture"]]];
+  for (int c = 0; c < ((images.count < kNumberOfImages)?images.count:kNumberOfImages); c++) {
+    [_imageViews[c] setImageWithURL:[NSURL URLWithString:images[c][@"picture"]]];
+  }
 }
 
 - (void) imageViewPressed:(UITapGestureRecognizer *)recoginizer {
-  
   NSDictionary *photo = _images[recoginizer.view.tag];
   [self.delegate facebookPhotoGridTableViewCell:self didSelectPhoto:photo withPreviewImage:[(UIImageView *)recoginizer.view image]];
-  
-  
-  
-
 }
+
+
+
 
 @end
